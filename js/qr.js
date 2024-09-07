944255edd8d8fef7ce0f2a63397812c524d763f8
Bensagra <Bensagra@gmail.com>
2024-09-07 14:59:52 -0300

    Adding delete from getTime

---
+++ js/qr.js
@@ -0,0 +1,8 @@
+    let text = document.getElementById("text")    
+    let res = localStorage.getItem("correctKey")
+    
+    let img = document.createElement("img")
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(JSON.parse(res).tokenId)}`
+    qr.appendChild(img)
+    console.log(JSON.parse(res).tokenId)
+    text.innerText = `El slot para el retiro es el ${JSON.parse(res).slot}`
